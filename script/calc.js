b87140fd822fb081645766a68db2cba992e12b6f
Reinaldo Braga <rodrigues.reinaldobraga@outlook.com>
2022-08-24 17:01:21 -0400

    Update script.
    Atualização da Lógica do funcionamento da calculadora.

--- script/calc.js
+++ script/calc.js
@@ -1,12 +1,41 @@
 const botoesNumeros = document.querySelectorAll(".botao__numeros"); 
 const botoesOperacoes = document.querySelectorAll(".botao__operacao");
 const tela = document.querySelector("input");
-const caracteres = ["/","*","+","-"];
+const botaoDecimal = document.querySelector(".botao__decimais")
+const casaDecimal1 = document.querySelector()
+const casaDecimal2 = document.querySelector()
+const casaDecimal3 = document.querySelector()
+const casaDecimal4 = document.querySelector()
+const caracteres = ["/","*","+","-","."];
+const operacoes = ["/","*","+","-"]
+let resultado = false;
+let iniciaZero = false;
 
 function ativaTecladoNumerico() {
     botoesNumeros.forEach(botao => {
         botao.addEventListener("click", () => {
-            tela.value += botao.value;
+            if(resultado && caracteres.includes(tela.value[(tela.value.length)-1]) == false) {
+                limpaTela();
+                resultado = false;
+                tela.value += botao.value;
+                if(tela.value==="0" && botao.textContent==="0") {
+                    tela.value = "0";
+                    iniciaZero = true;
+                }
+            } else {
+                resultado = false;
+                if(tela.value==="0" && botao.textContent==="0") {
+                    tela.value = "0";
+                } else if(tela.value==="0" && botao.textContent != "0") {
+                    tela.value = botao.textContent;
+                } else if(operacoes.includes(tela.value[tela.value.length-2]) && tela.value[tela.value.length-1] === "0" && botao.textContent != "."){
+                    let tamanho = tela.value.length;
+                    tela.value = tela.value.substring(0,tamanho-1);
+                    tela.value += botao.value;
+                } else { 
+                    tela.value += botao.value;
+                }
+            }
         })
     })
 }
@@ -17,20 +46,46 @@ function ativaTecladoCaracteres() {
             if(carc.textContent==="RESET") {
                 limpaTela();
             } else if(carc.textContent==="±") {
-                tela.value = tela.value*(-1);
+                if(tela.value==="") {
+                    limpaTela();
+                } else {
+                    const valorAntesOperacao = tela.value;
+                    tela.value = tela.value*(-1);
+                    if(tela.value==="NaN") {
+                        tela.value = valorAntesOperacao;
+                        alert("Atenção! Operação Inválida!");
+                    }
+                }
             } else if(carc.textContent==="÷") {
-                tela.value += "/";
-                naoRepeteCaracter();
+                if(tela.value==="") {
+                    limpaTela();
+                } else {
+                    tela.value += "/";
+                    naoRepeteCaracter();
+                }
             } else if(carc.textContent==="x") {
-                tela.value += "*";
-                naoRepeteCaracter();
+                if(tela.value==="") {
+                    limpaTela();
+                } else {
+                    tela.value += "*";
+                    naoRepeteCaracter();
+                }
             } else if(carc.textContent==="DEL") {
                 apagaTela();
             } else if(carc.textContent===".") {
-                if(tela.value.includes(".")) {
+                if(tela.value[tela.value.length-1] === ".") {
                     tela.value = tela.value;
                 } else {
                     tela.value += ".";
+                    naoRepeteCaracter();
+                }
+            } else if(carc.textContent==="=") {
+                if(caracteres.includes(tela.value[(tela.value.length)-1])) {
+                    const valorAntesOperacao = tela.value;
+                    tela.value = valorAntesOperacao;
+                    alert("Atenção! Operação Inválida!");
+                } else {
+                    realizaOperacao();
                 }
             } else {
                 tela.value += carc.textContent;
@@ -52,97 +107,34 @@ function apagaTela() {
 }
 
 function naoRepeteCaracter() {
-    for(let i=0; i < tela.value.length; i++) {
-        let novoCaracter = tela.value[i];
-        if(caracteres.includes(tela.value[i-1])) {
-            let tamanho = tela.value.length;
-            tela.value = tela.value.substring(0,tamanho-2);
-            tela.value += novoCaracter;
-            console.log(novoCaracter);
-        } else {
-            tela.value = tela.value;
-            console.log(novoCaracter);
+    let tamanho = tela.value.length;
+    let novoCaracter = tela.value[tamanho-1]
+    if(caracteres.includes(tela.value[tamanho-2])) {
+        tela.value = tela.value.substring(0,tamanho-2);
+        tela.value += novoCaracter;
+    }
+}
+
+function realizaOperacao() {
+    if(tela.value==="") {
+        tela.value = tela.value;
+    } else {
+        for(let i = 0; i < tela.value.length; i++) {
+            if(operacoes.includes(tela.value[i])) {
+                const calculo = eval(tela.value);
+                tela.value = calculo;
+                resultado = true;
+            } else {
+                tela.value = tela.value;
+            }
         }
     }
 }
 
-// function mudaCaracter() {
-//     for(let i=0; i < tela.value.length; i++) {
-//         if(caracteres.includes(tela.value[i])) {
-//             let tamanho = tela.value.length;
-//             tela.value = tela.value
-//         }
-//     }
-// }
+function ativaCalculadora() {
+    ativaTecladoNumerico();
+    ativaTecladoCaracteres();
+}
 
-ativaTecladoNumerico();
-ativaTecladoCaracteres();
-
-// function naoRepeteCaracteres() {
-//     for(let i=0; i < tela.value.length; i++) {
-//         if(tela.value[i-1]==="*" || tela.value[0]==="*") {
-//             let tamanho = tela.value.length;
-//             tela.value = tela.value.substring(0,tamanho-1);
-//         }
-//     }
-// }
-
-// function limpaTela() {
-//     tela.value = "";
-// }
-
-// function avisoErro() {
-//     if(tela.value==="NaN") {
-//         alert("Isso é um bug")
-//         limpaTela();
-//     }
-// }
-
-// function apagaTela() {
-//     if(tela.value.length >= 1) {
-//         let tamanho = tela.value.length;
-//         tela.value = tela.value.substring(0,tamanho-1);
-//     }
-// }
-
-// function naoRepeteSinal() {
-//     for(let i = 0; i < tela.value.length; i++) {
-//         if(tela.value) {
-//         } 
-//     }
-// }
-
-// function ativaTela() {
-//     botoes.forEach(botao => {
-//         botao.addEventListener("click", () => {
-//             if(botao.value===0) {
-//                 if(botao.textContent==="x") {
-//                     tela.value += "*"
-//                 } else if(botao.textContent==="RESET") {
-//                     limpaTela();
-//                 } else if(botao.textContent==="÷") {
-//                     tela.value += "/";
-//                 } else if(botao.textContent==="-") {
-//                     tela.value += "-"
-//                 } else if(botao.textContent==="+") {
-//                     tela.value += "+";
-//                 } else if(botao.textContent==="+/-") {
-//                     tela.value = tela.value*(-1);
-//                 } else if(botao.textContent===".") {
-//                     tela.value += ".";
-//                 } else if(botao.textContent==="DEL") {
-//                     apagaTela();
-//                 } else if(botao.textContent==="0") {
-//                     tela.value += 0;
-//                 }
-//             } else {
-//                 tela.value += botao.value;
-//             }
-//             avisoErro();
-//             naoRepeteSinal();
-//         })
-//     })
-// }
-
-// ativaTela();
+ativaCalculadora();
 
